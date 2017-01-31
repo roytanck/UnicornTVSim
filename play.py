c0345e5559e75c6f12c04b2d3c526e2dc9a5dd87
Roy Tanck <roy.tanck@gmail.com>
2017-01-31 20:28:51 +0100

    Added the actual Unicorn HAT code. Seems to run fine at 25fps on my Pi3.

--- play.py
+++ play.py
@@ -2,8 +2,15 @@
 
 from PIL import Image
 import datetime, time, threading
+import unicornhat as unicorn
 
 input_file = open("output.txt", "r")
+
+# start up the unicorn hat
+unicorn.set_layout(unicorn.HAT)
+unicorn.rotation(90)
+unicorn.brightness(1)
+unicorn.off()
 
 # read the file's content instead of using readline in aech iteration
 contents = input_file.read()
@@ -26,10 +33,15 @@ def processline():
 			time_passed = time.time() - starttime
 			starttime = time.time()
 			print( 250/time_passed )
-		index = index + 1
+		index = (index + 1) % len(lines)
 		# line = input_file.readline()
 		values = line.rstrip('\r\n').split(',')
 		# print ( 'R:' + str(values[0]) + ' G:' + str(values[1]) + ' B:' + str(values[2]) )
+		for y in range(8):
+			for x in range(8):
+				unicorn.set_pixel(x,y,int(values[0]),int(values[1]),int(values[2]))
+		unicorn.show()
+
 		next_call = next_call+0.04
 		time.sleep(next_call - time.time())
 
